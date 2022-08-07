b4241b11cf1e3326556d99be435537d68e8a6879
HUNSIKLEE <hunsik0206@gmail.com>
2022-08-07 20:51:08 +0900

    .

---
+++ board-app/app/srcxx/main/java/com/bitcamp/board/BoardListApp.java
@@ -0,0 +1,23 @@
+package com.bitcamp.board;
+
+public class BoardListApp {
+	public static void main(String[] args) {
+		System.err.println("[게시글 목록]");
+		System.err.println("번호  제목 조회수 작성자 등록일");
+
+		System.err.println("1");
+		System.err.println("\t");
+		System.err.println("제목입니다1");
+		System.err.println('t');
+		System.err.println(20 + "\t");
+		System.err.println("홍길동 \t");
+		System.err.println("2022-07-30\r\n");
+
+
+		System.err.print(2+ "\t" + "제목입니다2\t" + 11 + "\t" + "홍길동\t" + "2022-07-30\n");
+		System.err.println(3+ "\t" + "제목입니다3\t" + 11 + "\t" + "홍길동\t" + "2022-07-30");
+		System.err.printf("%d\t%s\t%d\t%s\t%s\n",4 ,"제목입니다4",45,"유관순","2022-07-30");
+
+	}
+
+}
