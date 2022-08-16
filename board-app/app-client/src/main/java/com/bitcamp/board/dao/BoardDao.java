26cacf6f7bac204eb8c5830f87a640ecf6fb9b3b
HunSik Lee <hunsik0206@gmail.com>
2022-08-16 22:24:40 +0900

    .

--- board-app/app-client/src/main/java/com/bitcamp/board/dao/BoardDao.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/dao/BoardDao.java
@@ -31,11 +31,11 @@ public class BoardDao {
         strBuilder.append(str);
       }
 
-      // String Builder 에 보관된 JSON  문자열을 가지고 BOARD[]을 생성한다.
+      // StringBuilder에 보관된 JSON 문자열을 가지고 Board[] 을 생성한다. 
       Board[] arr = new Gson().fromJson(strBuilder.toString(), Board[].class);
 
-      //Board[] 배열의 저장된 객체를 List로 옮긴다.
-      for(int i = 0; i < arr.length; i++) {
+      // Board[] 배열의 저장된 객체를 List 로 옮긴다.
+      for (int i = 0; i < arr.length; i++) {
         list.add(arr[i]);
       }
     }
