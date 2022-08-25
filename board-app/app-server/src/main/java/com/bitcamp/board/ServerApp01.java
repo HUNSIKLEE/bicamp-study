dc2c3f5e438f8cae482c4b10298a98d5784f9326
HunSik Lee <hunsik0206@gmail.com>
2022-08-25 20:46:16 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp01.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp01.java
@@ -1,11 +1,8 @@
 package com.bitcamp.board;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
-import java.util.Map;
 import com.bitcamp.board.servlet.BoardServlet;
 import com.bitcamp.board.servlet.MemberServlet;
 import com.bitcamp.servlet.Servlet;
@@ -43,45 +40,4 @@ public class ServerApp01 {
 
     System.out.println("서버 종료!");
   }
-  
-  public static class RequestThread extends Thread{
-
-    private Socket socket;
-    private Map<String,Servlet> servletMap;
-    
-    
-      public RequestThread(Socket socket, Map<String,Servlet> servletMap) {
-    
-        this.socket = socket;
-        this.servletMap = servletMap;
-      }
-      @Override
-      public void run() {
-        try (Socket socket = this.socket;
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
-
-          System.out.println("클라이언트와 연결 되었음!");
-
-          // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
-          String dataName = in.readUTF();
-
-          Servlet servlet = servletMap.get(dataName);
-          if (servlet != null) {
-            servlet.service(in, out);
-          } else {
-            out.writeUTF("fail");
-          }
-
-          System.out.println("클라이언트와 연결을 끊었음!");
-        } catch (Exception e) {
-          System.out.println("클라이언트 요청 처리 중 오류 발생");
-          e.printStackTrace();
-      }
-     }
-      
-      
-      
-  }
-
 }
