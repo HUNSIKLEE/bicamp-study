f84b3561b29ddd0a22fca214d7ae3409a994433d
HunSik Lee <hunsik0206@gmail.com>
2022-09-09 14:16:24 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/dao/MemberDao.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/MemberDao.java
@@ -1,49 +1,14 @@
 package com.bitcamp.board.dao;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import com.bitcamp.board.domain.Member;
-import com.google.gson.Gson;
+import com.bitcamp.util.LinkedList;
+import com.bitcamp.util.List;
 
 // 회원 목록을 관리하는 역할
 //
 public class MemberDao {
 
   List<Member> list = new LinkedList<Member>();
-  String filename;
-
-  public MemberDao(String filename) {
-    this.filename = filename;
-  }
-
-  public void load() throws Exception {
-    try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-
-      StringBuilder strBuilder = new StringBuilder();
-      String str;
-      while ((str = in.readLine()) != null) {
-        strBuilder.append(str);
-
-
-        Member[] arr = new Gson().fromJson(strBuilder.toString(), Member[].class);
-
-        for(int i = 0; i < arr.length; i++) {
-          list.add(arr[i]);
-        }
-      }
-    }
-  }
-
-  public void save() throws Exception {
-    try (FileWriter out = new FileWriter(filename)) {
-      Member[] members = list.toArray(new Member[0]);
-      out.write(new Gson().toJson(members));
-    }
-  }
 
   public void insert(Member member) {
     list.add(member);
@@ -70,15 +35,7 @@ public class MemberDao {
   }
 
   public Member[] findAll() {
-    Iterator<Member> iterator = list.iterator();
-
-    Member[] arr = new Member[list.size()];
-
-    int i = 0;
-    while (iterator.hasNext()) {
-      arr[i++] = iterator.next(); 
-    }
-    return arr;
+    return list.toArray(new Member[0]);
   }
 }
 
